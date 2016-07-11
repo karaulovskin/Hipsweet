086203ad1703857059f3cfa3b9d6e89d27ae188b
Karaulovskin <karaulovskin@yandex.ru>
2016-07-11 19:41:30 +0500

    supply_js_End

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
 (function () {
+
+
+	// SCROLL
+	$("[href='#order-form']").click(function () {
+		$("body, html").animate({
+			scrollTop: $('.order').offset().top - 110
+		}, 800);
+
+		return false;
+	});
+
+
+
+	// ACCORDION
 	$('.accordeon__trigger').on('click', function(e){
 		e.preventDefault();
 
@@ -25,7 +39,32 @@
 			item.removeClass('active');
 			currentContent.stop(true, true).slideUp();
 		}
-		
-
 	})
+
+
+	// API 2GIS
+		var map;
+
+	DG.then(function () {
+	    map = DG.map('map', {
+	        center: [54.98, 82.89],
+	        zoom: 13,
+            scrollWheelZoom: false,
+            fullscreenControl: false
+	    });
+
+
+	    myIcon = DG.icon({
+	        iconUrl: 'img/map/map-marker.png',
+	        iconSize: [42, 59]
+	    });
+
+	    DG.marker([54.98, 82.89], {
+	        icon: myIcon
+	    }).addTo(map).bindPopup('Hipsweet');
+	});
+
+
+
 })();
+
