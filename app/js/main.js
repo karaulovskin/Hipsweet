aee0bcdbf21c3d323c48ade56b45b2d5944e3e32
Karaulovskin <karaulovskin@yandex.ru>
2016-07-11 13:24:31 +0500

    accor_js_End

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,31 @@
-$ (function () {
-	console.log ('файл main.js успешно загружен')
-})
+(function () {
+	$('.accordeon__trigger').on('click', function(e){
+		e.preventDefault();
+
+		var
+			$this = $(this),
+			container = $this.closest('.accordeon__list'),
+			item = $this.closest('.accordeon__item'),
+			currentContent = item.find('.accordeon__text'),
+			duration = 500;
+
+		if(!item.hasClass('active')) {
+
+			item
+				.addClass('active')
+				.siblings()
+				.removeClass('active')
+				.find('.accordeon__text')
+				.stop(true, true)
+				.slideUp();
+
+			currentContent.stop(true, true).slideDown(duration);
+		} else {
+
+			item.removeClass('active');
+			currentContent.stop(true, true).slideUp();
+		}
+		
+
+	})
+})();
